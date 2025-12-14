606e3d286f837780572a2a4cb5cceda944bcedcb
avidersone-basket <avidersone@gmail.com>
2025-12-15 01:27:18 +0530

    Create amazonWishlist.js

---
+++ services/amazonWishlist.js
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+
+const wishlists = [
+  "2UXWTCZV1NEL2",
+  "KRF7Z0109CDU",
+  "24VB0A2VMIWFX",
+  "1A37JWTZP80MQ",
+  "2MW3NMKMDBM4B"
+];
+
+export function pickWishlist() {
+  return wishlists[Math.floor(Math.random() * wishlists.length)];
+}
+
+export async function clearList(listId) {
+  await fetch(`https://www.amazon.in/hz/wishlist/ls/${listId}/clear`);
+}
+
+export async function addToList(listId, asin, qty) {
+  await fetch(
+    `https://www.amazon.in/hz/wishlist/ls/${listId}/add?asin=${asin}&quantity=${qty}`
+  );
+}
+
+export function redirectUrl(listId) {
+  return `https://www.amazon.in/hz/wishlist/ls/${listId}?ref_=wl_share&tag=${process.env.AFFILIATE_TAG}`;
+}
