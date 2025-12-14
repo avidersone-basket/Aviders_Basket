cabe152f5373a4fa16876f365da7b2d29bff9136
avidersone-basket <avidersone@gmail.com>
2025-12-15 02:28:42 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
 dotenv.config();
-connectDB();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+connectDB();
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
